bfcc78e821178d6d0e34a36924529eef4ab7597e
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2022-08-05 15:51:12 -0500

    commit

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import LamaTube from "../img/logo.png";
+import NewTube from "../img/logo.png";
 import HomeIcon from "@mui/icons-material/Home";
 import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
 import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
@@ -92,8 +92,8 @@ const Menu = ({ darkMode, setDarkMode }) => {
       <Wrapper>
         <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
           <Logo>
-            <Img src={LamaTube} />
-            LamaTube
+            <Img src={NewTube} />
+            NewTube
           </Logo>
         </Link>
         <Item>
